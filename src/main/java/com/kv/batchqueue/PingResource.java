89dd035aae56b41d3dbe38f571247ac5fea69648
Vi <vanvibig@gmail.com>
2022-02-26 16:17:02 +0700

    init project + openapi + log debug

---
+++ src/main/java/com/kv/batchqueue/PingResource.java
@@ -0,0 +1,18 @@
+package com.kv.batchqueue;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class PingResource {
+
+    @GetMapping(value = "ping")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void ping() {
+        log.info("pong");
+    }
+}
---
+++ src/test/java/com/kv/batchqueue/BatchQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kv.batchqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
