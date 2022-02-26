4978a3d40bdb6a5d152295e09fa8f5251bac40c5
Vi <vanvibig@gmail.com>
2022-02-26 16:57:33 +0700

    add mq listener

---
+++ src/main/java/com/kv/batchqueue/MqListener.java
@@ -0,0 +1,17 @@
+package com.kv.batchqueue;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.Message;
+
+@Slf4j
+@Configuration
+public class MqListener {
+
+    @JmsListener(destination = "${custom.mq.destination}", containerFactory = "jmsListenerContainerFactory")
+    public void onMessage(Message<String> message) {
+        log.info("onMessage: {}", message.getPayload());
+    }
+
+}
